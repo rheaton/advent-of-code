2db0dcf8d84b77fc7ac1782a272836974f8258ed
Rachel Heaton <rheaton@workramp.com>
2023-12-04 18:24:03 -0800

    Part 2 day 2

--- 2023/day2/script.rb
+++ 2023/day2/script.rb
@@ -9,6 +9,7 @@ class Round
     @green = 0
     parse_round_text
   end
+
   attr_accessor :red, :green, :blue
 
   def playable?
@@ -36,12 +37,20 @@ class Game
   def initialize(game_text)
     @game_text = game_text
     @id = game_text.match(/Game (\d+):/)[1].to_i
+    @red = 0
+    @blue = 0
+    @green = 0
     parse_rounds
   end
-  attr_accessor :id
+
+  attr_accessor :id, :red, :green, :blue
 
   def playable?
     @rounds.all?(&:playable?)
+  end
+
+  def power
+    red * blue * green
   end
 
   private
@@ -49,7 +58,11 @@ class Game
   def parse_rounds
     @rounds = []
     @game_text.split(';').each do |round_text|
-      @rounds << Round.new(round_text)
+      round = Round.new(round_text)
+      @rounds << round
+      @red = [red, round.red].max
+      @blue = [blue, round.blue].max
+      @green = [green, round.green].max
     end
   end
 end
@@ -66,19 +79,41 @@ class ColourGameTest < Test::Unit::TestCase
   end
 
   def test_playable
-    assert_true(playable?("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
-    assert_true(playable?("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"))
-    assert_false(playable?("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
-    assert_false(playable?("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"))
-    assert_true(playable?("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
+    game1 = Game.new("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    assert_true(game1.playable?)
+    assert_equal([4, 2, 6], [game1.red, game1.green, game1.blue])
+    assert_equal(game1.power, 48)
+
+    game2 = Game.new("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")
+    assert_true(game2.playable?)
+    assert_equal([1, 3, 4], [game2.red, game2.green, game2.blue])
+    assert_equal(game2.power, 12)
+
+    game3 = Game.new("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")
+    assert_false(game3.playable?)
+    assert_equal([20, 13, 6], [game3.red, game3.green, game3.blue])
+    assert_equal(game3.power, 1560)
+
+    game4 = Game.new("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")
+    assert_false(game4.playable?)
+    assert_equal([14, 3, 15], [game4.red, game4.green, game4.blue])
+    assert_equal(game4.power, 630)
+
+    game5 = Game.new("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+    assert_true(game5.playable?)
+    assert_equal([6, 3, 2], [game5.red, game5.green, game5.blue])
+    assert_equal(game5.power, 36)
   end
 end
 
 playable = []
+powers = []
 File.foreach("#{File.dirname(__FILE__)}/input") do |line|
   g = Game.new(line)
   playable << g.id if g.playable?
+  powers << g.power
 end
-puts "Sum: #{playable.sum}"
+puts "Sum of game ids of playable games: #{playable.sum}"
+puts "Sum of powers of all games: #{powers.sum}"
 
 
